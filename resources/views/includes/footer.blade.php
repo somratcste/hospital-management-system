827b8279083af23d6d0aa3f5f69e310096e36ffb
somratcste <somratcste@gmail.com>
2016-07-30 10:07:12 +0600

    Nice works finally

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -352,36 +352,37 @@
     <!-- /chat -->
   </div>
 
+
   <!-- build:js({.tmp,app}) scripts/app.min.js -->
-  <script src="{{ asset('scripts/extentions/modernizr.js') }}"></script>
-  <script src="{{ asset('vendor/jquery/dist/jquery.js') }}"></script>
-  <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.js') }}"></script>
-  <script src="{{ asset('vendor/jquery.easing/jquery.easing.js') }}"></script>
-  <script src="{{ asset('vendor/fastclick/lib/fastclick.js') }}"></script>
-  <script src="{{ asset('vendor/onScreen/jquery.onscreen.js') }}"></script>
-  <script src="{{ asset('vendor/jquery-countTo/jquery.countTo.js') }}"></script>
-  <script src="{{ asset('vendor/perfect-scrollbar/js/perfect-scrollbar.jquery.js') }}"></script>
-  <script src="{{ asset('scripts/ui/accordion.js') }}"></script>
-  <script src="{{ asset('scripts/ui/animate.js') }}"></script>
-  <script src="{{ asset('scripts/ui/animate.js') }}"></script>
-  <script src="{{ asset('scripts/ui/panel-controls.js') }}"></script>
-  <script src="{{ asset('scripts/ui/preloader.js') }}"></script>
-  <script src="{{ asset('scripts/ui/toggle.js') }}"></script>
-  <script src="{{ asset('scripts/urban-constants.js') }}"></script>
-  <script src="{{ asset('scripts/extentions/lib.js') }}"></script>
+  <script src="scripts/extentions/modernizr.js"></script>
+  <script src="vendor/jquery/dist/jquery.js"></script>
+  <script src="vendor/bootstrap/dist/js/bootstrap.js"></script>
+  <script src="vendor/jquery.easing/jquery.easing.js"></script>
+  <script src="vendor/fastclick/lib/fastclick.js"></script>
+  <script src="vendor/onScreen/jquery.onscreen.js"></script>
+  <script src="vendor/jquery-countTo/jquery.countTo.js"></script>
+  <script src="vendor/perfect-scrollbar/js/perfect-scrollbar.jquery.js"></script>
+  <script src="scripts/ui/accordion.js"></script>
+  <script src="scripts/ui/animate.js"></script>
+  <script src="scripts/ui/link-transition.js"></script>
+  <script src="scripts/ui/panel-controls.js"></script>
+  <script src="scripts/ui/preloader.js"></script>
+  <script src="scripts/ui/toggle.js"></script>
+  <script src="scripts/urban-constants.js"></script>
+  <script src="scripts/extentions/lib.js"></script>
   <!-- endbuild -->
 
   <!-- page level scripts -->
-  <script src="{{ asset('scripts/extentions/lib.js') }}"></script>
-  <script src="{{ asset('vendor/rickshaw/rickshaw.min.js') }}"></script>
-  <script src="{{ asset('vendor/flot/jquery.flot.js') }}"></script>
-  <script src="{{ asset('vendor/flot/jquery.flot.resize.js') }}"></script>
-  <script src="{{ asset('vendor/flot/jquery.flot.categories.js') }}"></script>
-  <script src="{{ asset('vendor/flot/jquery.flot.categories.js') }}"></script>
+  <script src="vendor/d3/d3.min.js"></script>
+  <script src="vendor/rickshaw/rickshaw.min.js"></script>
+  <script src="vendor/flot/jquery.flot.js"></script>
+  <script src="vendor/flot/jquery.flot.resize.js"></script>
+  <script src="vendor/flot/jquery.flot.categories.js"></script>
+  <script src="vendor/flot/jquery.flot.pie.js"></script>
   <!-- /page level scripts -->
 
   <!-- initialize page scripts -->
-  <script src="{{ asset('scripts/pages/dashboard.js') }}"></script>
+  <script src="scripts/pages/dashboard.js"></script>
   <!-- /initialize page scripts -->
 
 </body>
