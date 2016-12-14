d77f0f883ae55d487d044f40a11df21c48db976d
somratcste <somratcste@gmail.com>
2016-12-14 15:13:45 +0600

    remove user pic

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -70,7 +70,7 @@
 
       <li>
         <a href="javascript:;" data-toggle="dropdown">
-          <img src="images/avatar.jpg" class="header-avatar img-circle ml10" alt="user" title="user">
+         <!--  <img src="images/avatar.jpg" class="header-avatar img-circle ml10" alt="user" title="user"> -->
           <span class="pull-left" style="text-transform:uppercase;">{{(Auth::user()->name)}}</span>
         </a>
         <ul class="dropdown-menu">
